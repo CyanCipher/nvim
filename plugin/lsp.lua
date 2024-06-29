23f6ef1f743e0b23bc4781f36aaee5f5e4c7bec4
Sudhanshu Pandey <heyitsmemomot@gmail.com>
2024-06-29 15:44:03 +0530

    Update lsp.lua

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -33,7 +33,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'tsserver', 'pyright', 'lua_ls', 'gopls', 'rust_analyzer', 'clangd' },
+    ensure_installed = { 'tsserver', 'pyright', 'lua_ls', 'rust_analyzer', 'clangd' },
     handlers = {
         lsp_zero.default_setup,
         lua_ls = function()
