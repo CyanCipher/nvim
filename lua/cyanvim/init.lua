cdabfe4e5fc084cdc361f8d169e537be939539ab
CyanCipher <heyitsmemomot@gmail.com>
2025-07-12 02:19:34 +0530

    minor changes

--- lua/cyanvim/init.lua
+++ lua/cyanvim/init.lua
@@ -2,7 +2,8 @@ require("cyanvim.plugins")
 require("cyanvim.core")
 require("configs.keymaps")
 
-vim.cmd("colorscheme nightfox")
+vim.cmd("colorscheme carbonfox")
+vim.api.nvim_set_hl(0, "Visual", { bg = "#8a8c8c", fg = "#232627" })
 
 vim.filetype.add({
     extension = {
